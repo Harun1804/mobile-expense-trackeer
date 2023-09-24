2f6fd79053c54168a3cc22d892351ff693054bf5
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-09-25 05:56:02 +0700

    add const to some widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -38,7 +38,7 @@ class _ExpensesState extends State<Expenses> {
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        duration: Duration(seconds: 3),
+        duration: const Duration(seconds: 3),
         content: const Text('Expense has been deleted') ,
         action: SnackBarAction(
           label: 'Undo',
@@ -71,7 +71,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          Text("The chart"),
+          const Text("The chart"),
           Expanded(child: mainContent)
         ],
       ),
