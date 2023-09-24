8b71a27c4f8119752f51fed8c2b888ed2cd9dfba
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-09-25 05:51:39 +0700

    add snackbar and undo action

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -31,13 +31,34 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense){
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: const Text('Expense has been deleted') ,
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      )
+    );
   }
   
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(child: Text('No expenses found. Start Adding some!'));
+
+    if (_registeredExpenses.isNotEmpty){
+      mainContent = ExpenseList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense);
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Expense Tracker"),
@@ -51,7 +72,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           Text("The chart"),
-          Expanded(child: ExpenseList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,))
+          Expanded(child: mainContent)
         ],
       ),
     );
